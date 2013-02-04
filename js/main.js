e47917eb424e47663d6af2ac25828c23508f69c7
Polina Viro <polina.viro@gmail.com>
2013-02-04 00:05:25 -0500

    Added .js file and TODO

--- js/main.js
+++ js/main.js
@@ -1,34 +1,26 @@
-ListService
-===========
+$( "#subbed" ).accordion({
+	collapsible : true,
+	heightStyle: "content"
+});
+$("#notSubbed").accordion({
+	collapsible : true,
+	heightStyle: "content"
+});
 
-Devfest 2013
-
-Daniel Newman '15
-Ben Caine '15
-Polina Viro '15
-Ved Tanavde '15
-Gabriel Blanco '15
-
-
-FRONTEND TODOs:
+/*TODOS: 
 
 make and maintain a JSON object that's 
 specific to the user that would hold all info for the 
 lists that a given user is subscribed to. this can be as 
-simple as a listing of some ID of the lists.
+simple as a listing of some ID of the lists
 
 a JSON that would have the full list of 
 all lists, with their ID + name + description. May choose
 to populate dynamically the lists, instead of hardcoding.
 
-enable dragging from one list to the other, update the 
-user JSON with this.
+enable dragging from one list to the other.
 
 sorting by different categories.
 
 search feature.
-
-
-BACKEND TODOs:
-
-populate this! 
+*/
