59ed3921821c776319f5ebabfc1476be0ebc8cf5
Polina Viro <polina.viro@gmail.com>
2013-02-07 13:51:36 -0500

    Adding script that gets all clubs
    returns array w/ dictionary that has name, url, description for each
    club.

---
+++ python/clubGetter.py
@@ -0,0 +1,29 @@
+import urllib
+import json
+import httplib
+import re
+
+def getAllClubs():
+    clubList = []
+    conn = urllib.urlopen('https://lists.columbia.edu/mailman/listinfo')
+    lines = conn.read().split("\n")
+    for l in range(len(lines)):
+        club = {}
+        if lines[l].find('<td><a href="listinfo') > 0:
+            start = lines[l].find('listinfo')+8
+            end = lines[l].find('"><strong>')
+            club['url'] = lines[l][start:end]
+
+            start = lines[l].find('<strong>')+8
+            end = lines[l].find('</strong>')
+            club['name'] = lines[l][start:end]
+
+            if lines[l+1].find('<em>') > 0:
+                club['desc'] = ''
+            else:
+                start = lines[l+1].find('<td>')+4
+                end = lines[l+1].find('</td>')
+                club['desc'] = lines[l+1][start:end]
+
+            clubList.append(club)
+    return clubList
