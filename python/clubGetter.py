52caf9ce134b25dafd8b3a3fcd9b20220df39f23
Polina Viro <polina.viro@gmail.com>
2013-02-08 13:15:22 -0500

    Modifications on the clubGetter + output file

--- python/clubGetter.py
+++ python/clubGetter.py
@@ -3,27 +3,30 @@ import json
 import httplib
 import re
 
-def getAllClubs():
-    clubList = []
-    conn = urllib.urlopen('https://lists.columbia.edu/mailman/listinfo')
-    lines = conn.read().split("\n")
-    for l in range(len(lines)):
-        club = {}
-        if lines[l].find('<td><a href="listinfo') > 0:
-            start = lines[l].find('listinfo')+8
-            end = lines[l].find('"><strong>')
-            club['url'] = lines[l][start:end]
 
-            start = lines[l].find('<strong>')+8
-            end = lines[l].find('</strong>')
-            club['name'] = lines[l][start:end]
+clubList = []
+conn = urllib.urlopen('https://lists.columbia.edu/mailman/listinfo')
+lines = conn.read().split("\n")
+for l in range(len(lines)):
+    club = {}
+    if lines[l].find('<td><a href="listinfo') > 0:
+        start = lines[l].find('listinfo')+8
+        end = lines[l].find('"><strong>')
+        club['url'] = lines[l][start:end]
 
-            if lines[l+1].find('<em>') > 0:
-                club['desc'] = ''
-            else:
-                start = lines[l+1].find('<td>')+4
-                end = lines[l+1].find('</td>')
-                club['desc'] = lines[l+1][start:end]
+        start = lines[l].find('<strong>')+8
+        end = lines[l].find('</strong>')
+        club['name'] = lines[l][start:end]
 
-            clubList.append(club)
-    return clubList
+        if lines[l+1].find('<em>') > 0:
+            club['desc'] = ''
+        else:
+            start = lines[l+1].find('<td>')+4
+            end = lines[l+1].find('</td>')
+            club['desc'] = lines[l+1][start:end]
+
+        clubList.append(club)
+
+out = open('bigList.txt', 'w')
+out.write(str(clubList))
+out.close()
